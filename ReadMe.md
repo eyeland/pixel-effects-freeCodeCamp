48c75a6e2280fd0b51703ec8ac64f636e3f67d14
eyeland <63192606+eyeland@users.noreply.github.com>
2023-01-11 15:09:52 -0500

    add readme

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# **Pixel Effects**
+
+## This Project is Based on a Tutorial Links below
+
+### Links Below
+
+#### **Video** https://www.youtube.com/watch?v=UoTxOVEecbI&t=1938s
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //declare gobal vars
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let particleArray = [];
@@ -9,115 +9,117 @@ let adjustY = 0;
 
 //handle mouse
 let mouse = {
-    x: null,
-    y: null,
-    radius: 150
-}
+  x: null,
+  y: null,
+  radius: 150,
+};
 
-window.addEventListener('mousemove', function(event){
-    mouse.x = event.x;
-    mouse.y = event.y;
-})
+window.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
 
-ctx.fillStyle = 'white';
-ctx.font = '30px Verdana';
-ctx.fillText('Love', 0, 30);
+ctx.fillStyle = "white";
+ctx.font = "30px Verdana";
+ctx.fillText("hi", 0, 30);
 const textCoordinates = ctx.getImageData(0, 0, 100, 100);
 
 //create particles
 class Particle {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.size = 3; 
-        this.baseX = this.x
-        this.baseY = this.y
-        //speed of particles
-        this.density = (Math.random() * 40) + 5;
-    }
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 3;
+    this.baseX = this.x;
+    this.baseY = this.y;
+    //speed of particles
+    this.density = Math.random() * 40 + 5;
+  }
 
-    draw() {
-        ctx.fillStyle = 'white';
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
-        ctx.closePath();
-        ctx.fill()
-    }
-    //calculate distace bewteen too points //add gravity
-    update() {
-        let dx = mouse.x - this.x;
-        let dy = mouse.y - this.y;
-        let distance = Math.sqrt(dx * dx + dy * dy)
-        let forceDirectionX = dx / distance;
-        let forceDirectionY = dy / distance;
-        let maxDistance = mouse.radius;
-        let force = (maxDistance - distance) / maxDistance;
-        let directionX = forceDirectionX * force * this.density;
-        let directionY = forceDirectionY * force * this.density;
+  draw() {
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+  }
+  //calculate distace bewteen too points //add gravity
+  update() {
+    let dx = mouse.x - this.x;
+    let dy = mouse.y - this.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+    let forceDirectionX = dx / distance;
+    let forceDirectionY = dy / distance;
+    let maxDistance = mouse.radius;
+    let force = (maxDistance - distance) / maxDistance;
+    let directionX = forceDirectionX * force * this.density;
+    let directionY = forceDirectionY * force * this.density;
 
-        if(distance < mouse.radius) {
-            this.x -= directionX;
-            this.y -= directionY;
-        }else{
-            if (this.x !== this.baseX) {
-                let dx = this.x - this.baseX;
-                this.x -= dx / 10;
-            }
-            if ( this.y !== this.baseY) {
-               let dy = this.y = this.baseY;
-               this.y -= dy / 10; 
-            }
-        }
+    if (distance < mouse.radius) {
+      this.x -= directionX;
+      this.y -= directionY;
+    } else {
+      if (this.x !== this.baseX) {
+        let dx = this.x - this.baseX;
+        this.x -= dx / 10;
+      }
+      if (this.y !== this.baseY) {
+        let dy = (this.y = this.baseY);
+        this.y -= dy / 10;
+      }
     }
+  }
 }
-console.log(textCoordinates.data)
+console.log(textCoordinates.data);
 //fill particleArray with objects
 function init() {
-    particleArray = [];
-   for (let y = 0, y2 = textCoordinates.height; y < y2; y++) {
-        for(let x = 0, x2 = textCoordinates.width; x < x2; x++) {
-            if(textCoordinates.data[(y * 4 * textCoordinates.width) + (x * 4) + 3] > 128) {
-                let positionX = x + adjustX;
-                let positionY = y + adjustY;
-                particleArray.push(new Particle(positionX * 12, positionY * 20))
-           }
-        }
-   }
-//     particleArray.push(new Particle(50, 50))
- }
+  particleArray = [];
+  for (let y = 0, y2 = textCoordinates.height; y < y2; y++) {
+    for (let x = 0, x2 = textCoordinates.width; x < x2; x++) {
+      if (
+        textCoordinates.data[y * 4 * textCoordinates.width + x * 4 + 3] > 128
+      ) {
+        let positionX = x + adjustX;
+        let positionY = y + adjustY;
+        particleArray.push(new Particle(positionX * 12, positionY * 20));
+      }
+    }
+  }
+  //     particleArray.push(new Particle(50, 50))
+}
 
 init();
-console.log(particleArray)
+console.log(particleArray);
 
 //redraw canvas every frame
 function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < particleArray.length; i++) {
-        particleArray[i].draw();
-        particleArray[i].update();
-    }
-    connect()
-    requestAnimationFrame(animate)
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].draw();
+    particleArray[i].update();
+  }
+  connect();
+  requestAnimationFrame(animate);
 }
-animate()
+animate();
 
 function connect() {
-    let opacityValue = 1;
-    for (let a = 0; a < particleArray.length; a++){
-        for(let b = a; b < particleArray.length; b++) {
-            let dx = particleArray[a].x - particleArray[b].x;
-            let dy = particleArray[a].y - particleArray[b].y
-            let distance = Math.sqrt(dx * dx + dy * dy);
-            opacityValue = 1 - (distance/32)
-            ctx.strokeStyle = 'rgba(255, 255, 255,' + opacityValue + ')';
+  let opacityValue = 1;
+  for (let a = 0; a < particleArray.length; a++) {
+    for (let b = a; b < particleArray.length; b++) {
+      let dx = particleArray[a].x - particleArray[b].x;
+      let dy = particleArray[a].y - particleArray[b].y;
+      let distance = Math.sqrt(dx * dx + dy * dy);
+      opacityValue = 1 - distance / 32;
+      ctx.strokeStyle = "rgba(255, 255, 255," + opacityValue + ")";
 
-            if(distance < 32) {
-                ctx.lineWidth = 2;
-                ctx.beginPath()
-                ctx.moveTo(particleArray[a].x, particleArray[a].y);
-                ctx.lineTo(particleArray[b].x, particleArray[b].y);
-                ctx.stroke();
-            }
-        }
+      if (distance < 32) {
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.moveTo(particleArray[a].x, particleArray[a].y);
+        ctx.lineTo(particleArray[b].x, particleArray[b].y);
+        ctx.stroke();
+      }
     }
+  }
 }
